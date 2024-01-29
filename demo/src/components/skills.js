14511317cbe33d8fc455c4f97e8e383d825868a9
chaitanyareddy9 <tetalachaitanya10704@gmail.com>
2024-01-29 10:43:51 +0530

    deployed wedsite

---
+++ demo/src/components/skills.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <div>
+      <h2>Skills</h2>
+      <ul>
+        <li>Intermediate level of Java Programming</li>
+        <li>Computer Gaming</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Skills;
---
+++ demo/src/components/student-info.js
@@ -0,0 +1,24 @@
+import React from 'react';
+const StudentInfo = () => {
+    return (
+        <div>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Roll No.</td>
+                        <td>2211CS010510</td>
+                    </tr>
+                    <tr>
+                        <td>Name</td>
+                        <td>CHAITANYA</td>
+                    </tr>
+                    <tr>
+                        <td>CGPA</td>
+                        <td>8.3</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+};
+export default StudentInfo;
